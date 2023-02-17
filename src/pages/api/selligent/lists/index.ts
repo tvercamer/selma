eed8227949c9b8a7264213e7bbf2243df58795c1
Thomas Vercamer <tvercame@gmail.com>
2023-02-17 12:11:17 +0100

    Get fields and relations per list

--- src/pages/api/selligent/lists/index.ts
+++ src/pages/api/selligent/lists/index.ts
@@ -1,20 +1,58 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
+import { getAllData, SelligentRequestOptions } from "@/utils/selligent.utils";
+
+interface IAdditionalInformation {
+  label: string;
+  URLs: string[];
+  key?: string;
+}
+
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  const options = {
-    headers: {
-      "Accept": "application/json",
-      "X-ApiKey": `${process.env.SELLIGENT_API_KEY}:${process.env.SELLIGENT_API_SECRET}`,
-    },
-  };
+  const organization =
+    request.query.organization || process.env.SELLIGENT_TEST_ORGANIZATION;
+  const axiosURL = `${process.env.SELLIGENT_BASE_URL}/organizations/${organization}/lists`;
 
-  const listResponse = await axios.get(`${process.env.SELLIGENT_BASE_URL}/organizations/${process.env.SELLIGENT_TEST_ORGANIZATION}/lists`,
-    options
-  );
+  switch (request.method) {
+    case "GET":
+      const listResponse = await axios.get(axiosURL, SelligentRequestOptions);
+      const lists = listResponse.data.lists;
 
-  response.status(200).json(listResponse.data);
+      // Get additinal information
+      const listInformationURLs = lists.map(
+        (list: any) => `${axiosURL}/${list.api_name}`
+      );
+      const listInformationData = await getAllData(listInformationURLs);
+      lists.map((list: any) => {
+        list.information = listInformationData.find(
+          (record: any) => record.api_name == list.api_name
+        );
+      });
+
+      // Get fields
+      for(const list of lists){
+        const fieldResponse = await axios.get(`${axiosURL}/${list.api_name}/fields`, SelligentRequestOptions);
+        const fieldsData = fieldResponse.data;
+        list.fields = fieldsData.fields;
+      }
+
+      // Get relations
+      for(const list of lists){
+        const relationResponse = await axios.get(`${axiosURL}/${list.api_name}/relations`, SelligentRequestOptions);
+        const relationData = relationResponse.data;
+        list.relations = relationData.relations;
+      }
+
+      // Return full DB structure
+      response.status(200).json(lists);
+      break;
+    default:
+      response.setHeader("Allow", ["GET", "POST"]);
+      response.status(405).end(`Method ${request.method} Not Allowed`);
+      break;
+  }
 }
--- src/utils/selligent.utils.ts
+++ src/utils/selligent.utils.ts
@@ -4,20 +4,27 @@ interface IParams {
     organization?: string;
 }
 
+export const SelligentRequestOptions = {
+    headers: {
+      Accept: "application/json",
+      "X-ApiKey": `${process.env.SELLIGENT_API_KEY}:${process.env.SELLIGENT_API_SECRET}`,
+    },
+  };
+
+async function fetchSelligentData(URL: string){
+    const response = await axios.get(URL, SelligentRequestOptions);
+    return response.data;
+}
+
+export function getAllData(URLs: string[]){
+    return Promise.all(URLs.map(URL => fetchSelligentData(URL)))
+}
+
 export async function getSelligentLists(params: IParams){
     const lists = await axios.request({
         method: "GET",
-        baseURL:"/api/selligent/lists/list-information",
+        baseURL:"/api/selligent/lists",
         params
     });
     return lists.data;
-}
-
-export async function fetchData(URL: string, options: any){
-    const repsonse = await axios.get(URL, options);
-    return await repsonse.data;
-}
-
-export function getAllData(URLs: string[], options: any){
-    return Promise.all(URLs.map(URL => fetchData(URL, options)))
 }
